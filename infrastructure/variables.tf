85bf6a1a81796077dc504960e897667df05ba5c9
Léonard Pasi <leonardpasi@gmail.com>
2025-07-06 13:53:34 +0200

    added terraform variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,24 @@
 variable "aws_region" {
     description = "region for aws resources"
     type = string
+    default = "eu-central-2"
+}
+
+variable "reddit_app_id" {
+    description = "needed for ingestion from subreddit"
+    sensitive = true
+    type = string
+}
+
+variable "reddit_app_secret" {
+    description = "needed for ingestion from subreddit"
+    sensitive = true
+    type = string
+
+}
+
+variable "openai_api_key" {
+    description = "needed for API calls to OpenAI for LLM-based relevance scoring"
+    sensitive = true
+    type = string
 }
