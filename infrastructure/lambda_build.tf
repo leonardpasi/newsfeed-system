ecd749c225f91137a7d329c1c8df3252ff90c253
Léonard Pasi <leonardpasi@gmail.com>
2025-07-07 19:15:15 +0200

    provisioning lambda function for storage to dynamodb with terraform

--- infrastructure/lambda_build.tf
+++ infrastructure/lambda_build.tf
@@ -55,3 +55,22 @@ resource "null_resource" "build_llm_scoring_lambda" {
     ]))
   }
 }
+
+# Build storage lambda
+resource "null_resource" "build_storage_lambda" {
+  provisioner "local-exec" {
+    command = "chmod +x build_lambda.sh && ./build_lambda.sh storage"
+    working_dir = path.module
+  }
+
+  triggers = {
+    code_hash = sha256(join("", [
+      for f in fileset("${path.module}/../lambda_functions/storage", "**/*") :
+      filesha256("${path.module}/../lambda_functions/storage/${f}")
+    ]))
+    shared_hash = sha256(join("", [
+      for f in fileset("${path.module}/../lambda_functions/shared", "**/*") :
+      filesha256("${path.module}/../lambda_functions/shared/${f}")
+    ]))
+  }
+}
